481cbbc37847eadf98661680cd3264e6271d9fdf
Tomson <447975373@qq.com>
2016-08-02 23:31:27 +0800

    增加了ChooseAreaActivity类，增加了choosearea.xml

---
+++ app/src/main/java/com/tomson/fullweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,19 @@
+package com.tomson.fullweather.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tomson.fullweather.R;
+
+/**
+ * Created by Tomson on 2016/8/2.
+ */
+public class ChooseAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_area);
+    }
+}
